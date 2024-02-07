4b03dfd84e88f6f3f442f62f22f9e0006cae37d4
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-07 22:10:13 +0000

    ConstVar

---
+++ basic-js/ConstVar.js
@@ -0,0 +1,11 @@
+const userId = "6751"
+let userName = "imtushaarr"
+var userPassword = "121212"
+userCity = "Chd"
+let userSate
+
+userName = "puneet"
+userPassword = "121212"
+userCity = "kharar"
+
+console.table([userId, userName, userPassword, userCity, userSate])
