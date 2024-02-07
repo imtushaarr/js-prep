c28906060334f1aae63beae1622df753f42a3eef
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-07 22:09:11 +0000

    datatype

---
+++ basic-js/datatype.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+let name = "Tushar"
+let age = 23
+let isLoggedIn = false
+let state = null;
+
+//Primitive data types :
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolen => true/false
+//null => standalon value
+//undefine => 
+//symbol => unique 
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
