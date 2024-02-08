066027392021c33f9252b2e03f9316da0df260fb
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-08 10:08:24 +0000

    Understand Comparision

---
+++ basic-js/comparision.js
@@ -0,0 +1,26 @@
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 == 2);
+// console.log(1 >= 2);
+// console.log(1 != 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //comparision value; in this syntext null covert in 0
+
+
+// undefined always flase result
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+// note : most of the cases, we avoid thes type of conversion in programing
+// main priority of code is cleanness, always try code clean and readable.
+
+
+// === 
+
+console.log("2" === 2);
