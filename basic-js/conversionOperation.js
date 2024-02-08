0151b95b68639912b8c87ff63af109bb4c95deed
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-08 09:03:29 +0000

    Conversion of DataType

---
+++ basic-js/conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "Tushar"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33as" = NaN (Not a Number)
+// True = 1; False = 0
+
+// let isLoggedIn = "Tushar"
+
+// let boolaznIsLoogedIn = Boolean(isLoggedIn)
+// console.log(boolaznIsLoogedIn)
+
+// 1 = true; 0 = false
+// "" = false
+//"Tushar" = true
+
+let someNumber = 33
+let strignNumber = String(someNumber)
+console.log( typeof strignNumber)
