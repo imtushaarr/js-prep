9a12330fbf4ed4acfc791ec23136953685ddca1e
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-08 21:34:55 +0000

    Number & Math

---
+++ basic-js/numandmath.js
@@ -0,0 +1,62 @@
+// const score = 2001
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 23.5234
+// console.log(otherNumber.toPrecision(4))
+
+// var numTwo = 10000000
+// console.log(numTwo.toLocaleString("en-in"))
+
+
+// OTP generate practice
+/* function generateOTP(limit){
+    var digit = '0123456789';
+    let OTP = ''
+    for (let i=0; i<limit; i++){
+        OTP += digit[Math.floor(Math.random()*10)];
+    }
+    return OTP;
+}
+console.log("Random Number Generator :", generateOTP(6) ); */
+
+// *********** Math ************
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.abs(7));
+
+// console.log(Math.round(7.2));
+// console.log(Math.ceil(7.2));
+// console.log(Math.floor(7.2));
+// console.log(Math.min(7.2,4,7,9,4));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+
+// Main Note
+// console.log(Math.floor(Math.random() * (min - min +1) + min))
+
+/* function generateOTP(limit){
+    var digit = '012345';
+    let OTP = ''
+    for (i=0; i<limit; i++){
+        OTP += digit[Math.floor(Math.random()*6)];
+    }
+    return OTP;
+}
+
+console.log("Your 6 digit OTP is: ", generateOTP(6)) */
