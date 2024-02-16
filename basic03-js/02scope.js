b23c11212de62f6be712de79a8b7e780e04a0d2d
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-16 09:36:17 +0000

    Scope More Concept

--- basic03-js/02scope.js
+++ basic03-js/02scope.js
@@ -4,9 +4,47 @@ if (true){  //block scope
     let a = 10
     const b = 20
     // var c = 30
-    console.log("INNER:", a)
+    // console.log("INNER:", a)
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "tushar"
+
+    function two(){
+        const website = "GitHub"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+one()
+
+if(true){
+    const username = "tushar"
+    if(username === "tushar"){
+        const website = " Github"
+        // console.log(username + website)
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+// interesting **************************
+
+console.log(addone(5))
+function addone (num){ //function
+    return num + 1
+}
+
+
+addtwo(5)
+const addtwo = function(num){   // function and also expression
+    return num + 2
+}
+
