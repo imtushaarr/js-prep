311d6744b9c36cab1d86a4d27f79e119a8536f46
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-18 22:11:09 +0000

    Truthy

--- control-flow04/truthy-falsey.js
+++ control-flow04/truthy-falsey.js
@@ -16,5 +16,17 @@ if (userEmail) {
 
 
 // falsey value
-
 // false, 0, -0, bigint 0n, "", null, undefined, NaN.
+
+// truthy value
+// "0" (0 under the string), 'flase', " "(space), [], {}, function(){}, 
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
