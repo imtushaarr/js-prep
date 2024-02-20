d875391606d733dcf06aea42deffc289c7c43605
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-20 18:11:52 +0000

    WhileDo Loop

---
+++ iteration05-js/whileDoLoop.js
@@ -0,0 +1,23 @@
+// While and Do loop
+
+let index = 0
+while (index <= 10){
+    // console.log(`value of index ${index}`);
+    index = index +2
+}
+
+let myArray = ["Tushar", "Yash", "Jatin", "Puneet"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr +1
+}
+
+// do while loop
+
+let score = 11
+do{
+    console.log(`score is ${score}`);
+    score++
+
+} while (score <= 10);
