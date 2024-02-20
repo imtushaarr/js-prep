06aeba01bbab69fb731937afa2b88c826b63d3a5
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-20 19:53:25 +0000

    Map

--- iteration05-js/forOfLoop3.js
+++ iteration05-js/forOfLoop3.js
@@ -11,9 +11,31 @@ for (const i of arr) {  // iterator = i (like that index); object is kis chiz ke
 
 const greetings = "hello world"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+    // console.log(`Each char is ${greet}`);
 }
 
 
 // maps:- its ike array (unique value)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('USA', "UNITED STATE OF AMERICA")
+map.set('FR', "FRANCE")
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'SM'
+}
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
 
