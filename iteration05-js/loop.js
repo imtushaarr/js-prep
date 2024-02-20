8d543006b0fb824f99f0a7e528cb250b8de59ffe
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-20 17:59:34 +0000

    BnC

--- iteration05-js/loop.js
+++ iteration05-js/loop.js
@@ -18,8 +18,20 @@ for (let i = 0; i <= 10; i++) {
 }
 
 let myArray = ["flash", "batman", "superman"]
-console.log(myArray.length);
+// console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
+}
+
+
+// keywords 
+// break and continue 
+
+for (let i = 1; i < 20; i++) {
+    if (i == 5){
+        console.log(`detected 5`);
+        break
+    }
+    console.log(`value of i is ${i}`);
 }
