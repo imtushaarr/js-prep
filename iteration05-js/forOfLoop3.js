776166c2292f5ebd318f671e4f82c2075df6acff
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-20 19:38:41 +0000

    Map

---
+++ iteration05-js/forOfLoop3.js
@@ -0,0 +1,19 @@
+// For Of Loop
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const i of arr) {  // iterator = i (like that index); object is kis chiz ke uper loop lagana h
+    // console.log(i);
+}
+
+const greetings = "hello world"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+}
+
+
+// maps:- its ike array (unique value)
+
