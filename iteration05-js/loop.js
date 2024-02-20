26b96a1b81a914a17f14b563e836f89ee1b014e5
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-20 18:02:06 +0000

    loop

--- iteration05-js/loop.js
+++ iteration05-js/loop.js
@@ -35,3 +35,11 @@ for (let i = 1; i < 20; i++) {
     }
     console.log(`value of i is ${i}`);
 }
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5){
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of i is ${i}`);
+}
