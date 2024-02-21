04daef38d3252b9525ef8cce45453d4803dbe910
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-21 18:15:48 +0000

    Key

--- iteration05-js/loopInObject4.js
+++ iteration05-js/loopInObject4.js
@@ -1,1 +1,18 @@
-const myObject
+const myObject = {
+    py: "python",
+    jv: "java",
+    r: "ruby",
+    js: "javascript"
+}
+
+for (const key in myObject) {
+    // console.log(myObject);
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut for ${myObject}`);
+}
+
+for (const key in myObject) {
+    // console.log(key)
+}
+
+
