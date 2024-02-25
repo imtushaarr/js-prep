25de940ac2d9e139998ed44fbfac8868ab5515c7
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-25 16:18:46 +0000

    Project 4

---
+++ projects-07/4-GuessNumber/krafcoding.js
@@ -0,0 +1,34 @@
+const ramdomNumber = parseInt(Math.random()*100 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+function validateGuess(guess){
+
+}
+
+function ckeckGuess(guess){
+
+}
+
+function displayGuess
+
+function displayMessage(message){
+
+}
+
+
+
+
+
