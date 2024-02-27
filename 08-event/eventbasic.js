ab88f6e592dcb4be97179cdfc4147875a0ee9ccc
Tushar Gupta <tusharguptagps@gmail.com>
2024-02-27 15:08:11 +0000

    Events

---
+++ 08-event/eventbasic.js
@@ -0,0 +1,7 @@
+// attachEvent()
+    // JQuery - onEventLister
+
+    // Type, timestamp, defaultPrevented
+    // target, toElement, srcElement, currentTarget
+    //clicentX, clicentY, screenX, screenY
+    // altkey, ctrkey, shiftKey, keyCode
