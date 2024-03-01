b7c985ebe7aecfee6e6bc695d81376c416b23e9f
Tushar Gupta <tusharguptagps@gmail.com>
2024-03-01 22:11:13 +0000

    Promise

--- 11-advanceOne/promises.js
+++ 11-advanceOne/promises.js
@@ -2,31 +2,50 @@ const promiseOne = new Promise(function(resolve, reject){         // promise get
     // Do an async task
     // DB Calls, cryptography, network
     setTimeout(function(){
-        console.log('Async task is completed');
+        // console.log('Async task is completed');
         resolve()    // that is use to connect .then
     }, 1000)
 }) 
 
 promiseOne.then(function(){
-    console.log("Promise consumed");
+    // console.log("Promise consumed");
 })
 
 // combine both, both are same
 
 new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log("Async task 2");
+        // console.log("Async task 2");
         resolve()
     },1000)
 }).then(function(){
-    console.log("Async 2 resolve");
+    // console.log("Async 2 resolve");
 })
 
 
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
-
+        resolve({username: "Tushar", email: "tushar@krafcoding.com"})
     },1000)
 })
 
-promiseThree
+promiseThree.then(function(user){
+    // console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        if(!error){
+            resolve({username: "tushar", password: "123"})
+        } else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+const username = promiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+})
+
+console.log(username);
