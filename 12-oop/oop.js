4b8d11ea7257259c845367fd1c79b775883127fc
Tushar Gupta <tusharguptagps@gmail.com>
2024-03-04 20:38:29 +0000

    Constructor function

--- 12-oop/oop.js
+++ 12-oop/oop.js
@@ -11,6 +11,18 @@ const user = {
 
 // *********Constructor function*********
 
-
-
 // const promise = new Promise()      // new keyword that is called Constructor 
+
+function User(username, logincount, isLoggedIn){
+    this.username = username
+    this.logInCount = logincount
+    this.isLoggedIn = isLoggedIn
+
+    return this
+}
+
+const userOne = new User("tushar", 12, true)
+const userTwo = new User("krafcoding", 11, false)
+console.log(userOne);
+console.log(userTwo);
+
