07c8d82ff023e380b330b5e418046c6362c396e0
Tushar Gupta <tusharguptagps@gmail.com>
2024-03-07 16:47:51 +0000

    Static Properties

--- 12-oop/inheritance.js
+++ 12-oop/inheritance.js
@@ -12,9 +12,20 @@ class User{
 class Teacher extends User{
     constructor(username, email, password){
         super(username)
-        this.email = email;
-        this.password = password;
+        this.email = email
+        this.password = password
     }
 
-    
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
 }
+
+const tushar = new Teacher("Tushar", "tushar@teacher.com", "123")
+
+tushar.logMe()
+const agustya = new User("agutsya")
+
+agustya.logMe()
+
+console.log(tushar instanceof User);
